d53cd8780772039127d67bfca3f440a5cb5d6fbe
DavidThorpe71 <davidthorpe878@gmail.com>
2024-05-03 19:21:24 +0100

    adds initial repl

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/DavidThorpe71/monkey/repl"
+	"os"
+	user "os/user"
+)
 
+func main() {
+	u, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", u.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
